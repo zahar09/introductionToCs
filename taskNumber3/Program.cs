e9214d0028a279782eb360df29806e933c94c21d
Zakharov Ilia <ilya.zaharov.2009@mail.ru>
2023-06-26 11:45:49 +0300

    Решил задачу 3

---
+++ taskNumber3/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Clear();
+Console.Write("Введите число: ");
+int num = int.Parse(Console.ReadLine());
+
+if(num % 7 == 0 && num % 23 == 0){
+    Console.WriteLine($"Число {num} кратоно 7 и 23");
+}
+else{
+    Console.WriteLine($"Число {num} некратоно 7 и 23");
+}
