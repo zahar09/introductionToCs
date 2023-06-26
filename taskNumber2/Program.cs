b6c95cbef6ecf8006e4b1ec518be34c9cf28b51f
Zakharov Ilia <ilya.zaharov.2009@mail.ru>
2023-06-26 10:55:47 +0300

    Решил задачу 2

---
+++ taskNumber2/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+int num = new Random().Next(1, 100);
+int num1 = new Random().Next(1, 10);
+Console.WriteLine(num);
+Console.WriteLine(num1);
+int remainder = num % num1;
+if(remainder == 0){
+    Console.WriteLine($"Число {num} кратно {num1}");
+}
+else{
+    Console.WriteLine($"Число {num} не кратно {num1}, остаток {remainder}");
+}
