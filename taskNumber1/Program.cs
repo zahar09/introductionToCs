12d3fb210596a4908675bd56b706ac41b8b52350
Zakharov Ilia <ilya.zaharov.2009@mail.ru>
2023-06-26 10:27:48 +0300

    Решил задачу 1

---
+++ taskNumber1/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+int num = new Random().Next(100, 1000);
+//int num2 = (num % 100) / 10;
+Console.WriteLine(num);
+int num1 = num / 100;
+int num3 = num % 10;
+int ans = num1 * 10 + num3;
+Console.WriteLine(ans);
